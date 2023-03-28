f6f2a6da667e55d8a063fc31ddd190857ebe61b7
SumeetHaryani <sumeetharyani3@gmail.com>
2023-03-28 11:32:52 +0530

    Add CRUD api routes

---
+++ src/routes/api/comments/[commentId]/+server.js
@@ -0,0 +1,26 @@
+import { json } from '@sveltejs/kit';
+import { comments } from '$lib/comments';
+
+export function GET(requestEvent) {
+	const { params } = requestEvent;
+	const { commentId } = params;
+	const comment = comments.find((comment) => comment.id === parseInt(commentId));
+	return json(comment);
+}
+
+export async function PATCH(requestEvent) {
+	const { params, request } = requestEvent;
+	const { commentId } = params;
+	const { text } = await request.json();
+	const comment = comments.find((comment) => comment.id === parseInt(commentId));
+	comment.text = text;
+	return json(comment);
+}
+
+export async function DELETE(requestEvent) {
+	const { params } = requestEvent;
+	const { commentId } = params;
+	const comment = comments.find((comment) => comment.id === parseInt(commentId));
+	comments.splice(comments.indexOf(comment), 1);
+	return json(comment);
+}
