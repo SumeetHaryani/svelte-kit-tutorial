52b09437ede4abb4b944bc6064adfae0126939f3
SumeetHaryani <sumeetharyani3@gmail.com>
2023-03-29 23:04:51 +0530

    Add error handling

--- src/routes/products/[productId=integer]/+page.server.js
+++ src/routes/products/[productId=integer]/+page.server.js
@@ -1,11 +1,17 @@
+import { error, redirect } from '@sveltejs/kit';
 export const load = async (loadEvent) => {
-    const { fetch, params } = loadEvent;
-    const { productId } = params;
-    const title = 'Product details';
-    const response = await fetch(`http://localhost:3000/products/${productId}`);
-    const product = await response.json();
-    return {
-        title,
-        product
-    };
+	const { fetch, params } = loadEvent;
+	const { productId } = params;
+	if (productId > 3) {
+		// console.log('productId > 3');
+		// throw error(404, 'Product not found');
+		throw redirect(307, '/products');
+	}
+	const title = 'Product details';
+	const response = await fetch(`http://localhost:3000/products/${productId}`);
+	const product = await response.json();
+	return {
+		title,
+		product
+	};
 };
